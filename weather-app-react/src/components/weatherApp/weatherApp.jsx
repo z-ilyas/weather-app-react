6753e59cc48c0557b39b883211621125653bf0ba
Ilyas Mohamed <zswaleh@ymail.com>
2023-10-03 17:20:25 -0500

    cloud, humidty and wind icons show up on the browser

--- weather-app-react/src/components/weatherApp/weatherApp.jsx
+++ weather-app-react/src/components/weatherApp/weatherApp.jsx
@@ -23,7 +23,27 @@ function weatherApp() {
                     <img src={search_icon} alt="" />
                 </div>
             </div>
-        
+            <div className="weather-image">
+                <img src={cloud_icon} alt="" />
+            </div>
+            <div className="weather-temp">24°c</div>
+            <div className="weather-location">London</div>
+            <div className="data-container">
+                <div className="element">
+                    <img src={humidity_icon} alt="" className="icon" />
+                    <div className="data">
+                        <div className="humidity-percent">64%</div>
+                        <div className="text">Humidity</div>
+                    </div>
+                </div>
+                <div className="element">
+                <img src={wind_icon} alt="" className="icon" />
+                    <div className="data">
+                        <div className="wind-speed">10 m/h</div>
+                        <div className="text">Wind Speed</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
