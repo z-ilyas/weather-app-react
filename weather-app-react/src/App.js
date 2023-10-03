68e1045277b196e6ee06daa6ab93cde92bbd3ff8
Ilyas Mohamed <zswaleh@ymail.com>
2023-10-03 17:07:14 -0500

    downloaded new pics files, made the general view of the weather app with css

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import WeatherApp from './components/weatherApp/weatherApp.jsx';
 
 function App() {
   return (
     <div className="App">
-       
+       <WeatherApp/>
     </div>
   );
 }
--- weather-app-react/src/components/weatherApp/weatherApp.jsx
+++ weather-app-react/src/components/weatherApp/weatherApp.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-import './weatherApp.css'
+import './weatherApp.css';
+
+import search_icon from '../assets/search.png';
+import clear_icon from '../assets/clear.png';       
+import cloud_icon from '../assets/cloud.png';
+import drizzle_icon from '../assets/drizzle.png';
+import rain_icon from '../assets/rain.png';
+import snow_icon from '../assets/snow.png';
+import wind_icon from '../assets/wind.png';
+import humidity_icon from '../assets/humidity.png';
+
+
 function weatherApp() {
     return (
-        <>
-
-        </>
+        <div className="container">
+            <div className="top-bar">
+                <input  type="text" 
+                        classname="cityInput"
+                        placeholder="search"
+                />     
+                <div className="search-icon">
+                    <img src={search_icon} alt="" />
+                </div>
+            </div>
+        
+        </div>
     );
 }
     
