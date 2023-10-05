539ced85c8b02348e4181d11bb6f757555328cb1
Ilyas Mohamed <zswaleh@ymail.com>
2023-10-05 14:40:57 -0500

    function for searching for any city in the API has been created.

--- weather-app-react/src/components/weatherApp/weatherApp.jsx
+++ weather-app-react/src/components/weatherApp/weatherApp.jsx
@@ -15,6 +15,20 @@ function weatherApp() {
 
     let api_key = "693b44653e5ac79a8b6fbd69b9a1c614";
 
+    const search = async () => {
+        const element = document.getElementsByClassName("cityInput")
+        if(element[0].value===""){
+            return 0;
+        }
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Imperial&appid=${api_key}`;
+
+        let response = await fetch(url);
+
+        let data = await response.json();
+
+    
+    }   
+
     return (
         <div className="container">
             <div className="top-bar">
@@ -22,7 +36,7 @@ function weatherApp() {
                         classname="cityInput"
                         placeholder="search"
                 />     
-                <div className="search-icon">
+                <div className="search-icon" onClick={() => {search()}}> 
                     <img src={search_icon} alt="" />
                 </div>
             </div>
